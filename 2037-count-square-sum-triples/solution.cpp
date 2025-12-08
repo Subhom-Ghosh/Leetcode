a5435bf8c46a3c8c5741a8ac0303503d8136aa00
Subhom Ghosh <subhomghosh836@gmail.com>
2025-12-08 16:37:01 +0000

    Sync LeetCode submission Runtime - 10 ms (39.40%), Memory - 7.8 MB (88.09%)

---
+++ 2037-count-square-sum-triples/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int count=0;
+        for(int a=1;a<n;a++){
+            for (int b=1;b<n;b++){
+                int squareSum = ((a*a)+(b*b));
+                int c = sqrt(squareSum);
+                if(c<=n && (c*c)==squareSum){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
