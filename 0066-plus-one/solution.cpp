81de96b2fb92c0b7d028107764d16b64592fc6c4
Subhom Ghosh <subhomghosh836@gmail.com>
2026-01-17 15:15:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (28.38%)

---
+++ 0066-plus-one/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        // Start from last digit
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;     // simple increment
+                return digits;   // done
+            }
+            digits[i] = 0;       // 9 + 1 = 10 → put 0, carry continues
+        }
+
+        // If all digits were 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
