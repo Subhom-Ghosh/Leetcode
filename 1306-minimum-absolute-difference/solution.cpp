ba8b3c7b0cdb142fe1237562e3d8a6af6fc69afa
Subhom Ghosh <subhomghosh836@gmail.com>
2026-01-26 14:33:04 +0000

    Sync LeetCode submission Runtime - 15 ms (75.68%), Memory - 36.6 MB (75.31%)

---
+++ 1306-minimum-absolute-difference/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        int min_diff=INT_MAX;
+        for(int i=1;i<n;i++){
+            min_diff=min(min_diff,arr[i]-arr[i-1]);
+        }
+        vector<vector<int>> result;
+        for(int i=1;i<n;i++){
+            if (min_diff==arr[i]-arr[i-1]){
+                result.push_back({arr[i-1],arr[i]});
+            }
+        }
+        return result;
+    }
+};
