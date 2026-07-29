2fe87aa6f44358aeff507968fe62a80eca6e544e
Subhom Ghosh <subhomghosh836@gmail.com>
2026-07-29 18:22:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (79.65%)

--- 0151-reverse-words-in-a-string/solution.cpp
+++ 0151-reverse-words-in-a-string/solution.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        string ans="";
-        reverse(s.begin(),s.end());
-        for(int i=0;i<n;i++){
-            string word="";
-            while(i<n && s[i]!=' '){
-                word+=s[i];
-                i++;
+        int i = s.size()-1;
+
+        string result = "";
+
+        while(i>=0){
+            while(i>=0 && s[i]==' '){
+                i--;
             }
-            reverse(word.begin(),word.end());
-            if(word.length()>0){
-                ans+=" "+word;
+            if(i<0) break;
+            int end = i;
+
+            while(i>=0 && s[i]!=' '){
+                i--;
             }
+            string word = s.substr(i+1,end-i);
+
+            if(!result.empty())
+                result+=" ";
+
+            result+=word;
         }
-        return ans.substr(1);
+        return result;
+
     }
 };
